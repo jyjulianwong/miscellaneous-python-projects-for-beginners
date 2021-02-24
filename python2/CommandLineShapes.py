a96c76d9b16fcd554f6087b27d3f3ace73a221ce
Julian Wong <33598849+jyjulianwong@users.noreply.github.com>
2021-02-24 21:31:01 +0800

    Create CommandLineShapes.py

---
+++ python2/CommandLineShapes.py
@@ -0,0 +1,29 @@
+def drawAHeart():
+	print(" ***   *** ")
+	print("***********")
+	print("  *******  ")
+	print("   *****   ")
+	print("     *     ")
+
+def drawAKite():
+	print("     *     ")
+	print("   *****   ")
+	print(" ********* ")
+	print("***********")
+	print(" ********* ")
+	print("   *****   ")
+	print("     *     ")
+
+# TODO: Create some more shapes of your own by making functions like the ones above!
+
+def drawSomeHearts(n):
+	# TODO: Draw n number of hearts. Hint: A for-loop maybe?
+
+def drawSomeKites(n):
+	# TODO: Draw n number of kites. Hint: A for-loop maybe?
+
+# TODO: Create some more functions that print out shapes multiple times. 
+
+# TODO: Ask the user what shape they want. 
+# TODO: Ask the user how many times they want the shape to be printed. 
+# TODO: Use the functions above to print the correct number of shapes! 
